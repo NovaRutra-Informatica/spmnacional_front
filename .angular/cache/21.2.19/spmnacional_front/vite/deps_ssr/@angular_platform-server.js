1f9d63e2571dbfe44e3ea950b03975502f0520dd
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2026-08-26 11:36:30 -0400

    :bricks: ci: updating branch

---
+++ .angular/cache/21.2.19/spmnacional_front/vite/deps_ssr/@angular_platform-server.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BEFORE_APP_SERIALIZED,
+  DominoAdapter,
+  ENABLE_DOM_EMULATION,
+  INITIAL_CONFIG,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS,
+  PlatformState,
+  SERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS,
+  ServerModule,
+  VERSION,
+  isHostAllowed,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderInternal,
+  renderModule
+} from "./chunk-CGRQGQHI.js";
+import "./chunk-5CJB733P.js";
+import "./chunk-TD3CMAQY.js";
+import "./chunk-YG3ELG3R.js";
+import "./chunk-ZBQXT5RQ.js";
+import "./chunk-S6STQDT5.js";
+export {
+  BEFORE_APP_SERIALIZED,
+  INITIAL_CONFIG,
+  PlatformState,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule,
+  DominoAdapter as ɵDominoAdapter,
+  ENABLE_DOM_EMULATION as ɵENABLE_DOM_EMULATION,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS,
+  SERVER_CONTEXT as ɵSERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS,
+  isHostAllowed as ɵisHostAllowed,
+  renderInternal as ɵrenderInternal
+};
---
+++ .angular/cache/21.2.19/spmnacional_front/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-G7IX3XKQ.js";
+import "./chunk-CGRQGQHI.js";
+import "./chunk-CPYENFSX.js";
+import "./chunk-5CJB733P.js";
+import "./chunk-TD3CMAQY.js";
+import "./chunk-YG3ELG3R.js";
+import "./chunk-ZBQXT5RQ.js";
+import "./chunk-S6STQDT5.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
